5853b48baa37506a82ac379d2dba315eabffc06e
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-03-22 12:06:53 +0200

    [ADD] New module for using invoice address on contract

---
+++ contract_invoice_address/models/account_analytic_account.py
@@ -0,0 +1,16 @@
+from odoo import api, models
+
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = 'account.analytic.account'
+
+    @api.multi
+    def _prepare_sale(self):
+        # Super will handle ensure_one() and other validation
+        res = super(AccountAnalyticAccount, self)._prepare_sale()
+
+        # Add invoice id to writable values
+        if self.partner_invoice_id:
+            res['partner_invoice_id'] = self.partner_invoice_id.id
+
+        return res
