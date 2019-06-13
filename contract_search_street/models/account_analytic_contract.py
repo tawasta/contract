e0dbd97e75b8c0da1c93783ddbaaa7868cf54cd0
difbit <timo.kekalainen@hotmail.com>
2019-06-13 14:44:27 +0300

    [ADD] The Python-file

---
+++ contract_search_street/models/account_analytic_contract.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import api, fields, models
+
+
+class AccountAnaluticContract(models.Model):
+
+    _inherit = 'account.analytic.contract'
+
+    partner_id_street = fields.Char(
+        string='Street',
+        related='partner_id.street',
+    )
