c2bca288875a57dc859ac0a433205af373123c7e
difbit <36134879+difbit@users.noreply.github.com>
2019-07-05 14:21:35 +0300

    10.0 customer details (#6)
    * [ADD] The Readme-file
    
    * [ADD] The init-file
    
    * [ADD] The module descriptor file
    
    * [ADD] Empty translation-file
    
    * [ADD] The __init__.py file specifies to Python files to be included in the module
    
    * [ADD] Python-file
    
    * [ADD] Image-file
    
    * [ADD] View-file
    
    * [IMP] Removed api

---
+++ contract_customer_address_details/models/account_analytic_contract.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import fields, models
+
+
+class AccountAnaluticContract(models.Model):
+
+    _inherit = 'account.analytic.contract'
+
+    partner_id_street = fields.Char(
+        string='Street',
+        related='partner_id.street',
+    )
+
+    partner_id_street2 = fields.Char(
+        string='Street2',
+        related='partner_id.street2',
+    )
+
+    partner_id_zip = fields.Char(
+        string='Zip',
+        related='partner_id.zip',
+    )
+
+    partner_id_city = fields.Char(
+        string='City',
+        related='partner_id.city',
+    )
