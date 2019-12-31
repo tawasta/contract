b145ab01e4d2c65fec93d0a0b97ec23e285ffef7
retropikzel <retropikzel@mailbox.org>
2019-12-31 11:01:55 +0200

    [ADD] Added partner and contract to list view, removed auto-price? and unit of measure

---
+++ contract_line_list_view/models/contract_line.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+
+class ContractLineBetterContext(models.Model):
+
+    _inherit = 'contract.line'
+
+    partner_id = fields.Many2one(
+        string='Member',
+        comodel_name='res.partner',
+        related='contract_id.partner_id',
+        store=True,
+    )
