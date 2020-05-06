81a651ccdb2a4ace6d01e76b3cd85af00eadd011
retropikzel <retropikzel@mailbox.org>
2020-05-06 14:07:13 +0300

    [FIX] Get data from shipping partner

--- contract_invoice_shipping_line_view/models/account_analytic_invoice_line.py
+++ contract_invoice_shipping_line_view/models/account_analytic_invoice_line.py
@@ -14,3 +14,37 @@ class AccountAnalyticInvoiceLine(models.Model):
         related='analytic_account_id.partner_shipping_id',
         store=True,
     )
+
+    analytic_id_date_end = fields.Date(
+        string='End date',
+        related='analytic_account_id.date_end',
+        store=True,
+    )
+
+    analytic_id_shipping_street = fields.Char(
+        string='Street',
+        related='analytic_account_id.partner_shipping_id.street',
+    )
+
+    analytic_id_shipping_street2 = fields.Char(
+        string='Street 2',
+        related='analytic_account_id.partner_shipping_id.street2',
+    )
+
+    analytic_id_shipping_zip = fields.Char(
+        string='Zip',
+        related='analytic_account_id.partner_shipping_id_zip',
+    )
+
+    analytic_id_shipping_city = fields.Char(
+        string='City',
+        related='analytic_account_id.partner_shipping_id.city',
+        store=True,
+    )
+
+    analytic_id_shipping_country_id = fields.Many2one(
+        comodel_name='res.country',
+        string='Country',
+        related='analytic_account_id.partner_shipping_id.country_id',
+        store=True,
+    )
