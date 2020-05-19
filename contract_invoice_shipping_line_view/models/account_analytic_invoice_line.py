5c21ce5ffc9d860480832d10c5667e6156a6d888
Jarmo Kortetjärvi <jarmokortetjarvi@users.noreply.github.com>
2020-05-19 16:11:05 +0300

    [IMP] Add commercial partner and name

--- contract_invoice_shipping_line_view/models/account_analytic_invoice_line.py
+++ contract_invoice_shipping_line_view/models/account_analytic_invoice_line.py
@@ -21,6 +21,15 @@ class AccountAnalyticInvoiceLine(models.Model):
         store=True,
     )
 
+    analytic_id_name = fields.Char(
+        related='analytic_account_id.partner_shipping_id.name',
+    )
+    
+    analytic_id_commercial_partner_id = fields.Many2one(
+        comodel_name='res.partner',
+        related='analytic_account_id.partner_shipping_id.commercial_partner_id',
+    )
+    
     analytic_id_shipping_street = fields.Char(
         string='Street',
         related='analytic_account_id.partner_shipping_id.street',
