c6f26c31c2213bb22e6c0e3666a904bc84ccaaba
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2021-12-21 14:43:46 +0200

    Added translations

--- contract_send_messages/models/contract.py
+++ contract_send_messages/models/contract.py
@@ -9,7 +9,8 @@ class ContractContract(models.Model):
     def create(self, vals_list):
         records = super().create(vals_list)
         partners = self.env['website'].get_current_website().contract_email_ids
-        for rec in records:
-            rec.message_subscribe(partner_ids=partners.ids)
+        if partners:
+            for rec in records:
+                rec.message_subscribe(partner_ids=partners.ids)
 
         return records
