9effd527b2f3b225edae83d49c4b1bcfef7c1df6
Miika Nissi <miika@miikanissi.com>
2022-01-20 19:00:20 +0200

    [ADD] module to invoice member without company

---
+++ contract_mass_action_invoice_member/wizard/invoice_member_wizard.py
@@ -0,0 +1,61 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2022- Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from odoo import fields, models
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class InvoiceMemberWizard(models.TransientModel):
+    # 1. Private attributes
+    _name = "contract.mass.action.invoice.member.wizard"
+    _description = "Action to set default invoice address and create new invoice"
+
+    # 2. Fields declaration
+    contract_ids = fields.Many2many(
+        "contract.contract",
+        default=lambda self: self.env["contract.contract"].browse(
+            self._context.get("active_ids")
+        ),
+        string="Contracts",
+    )
+
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+    def action_invoice_member(self):
+        pass
+
+    # 8. Business methods
