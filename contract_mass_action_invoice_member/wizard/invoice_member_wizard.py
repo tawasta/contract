a90fa8fe7e61ae3caafeea98a97756ef641dfc99
Miika Nissi <miika@miikanissi.com>
2022-01-21 20:05:32 +0200

    [IMP] Improved invoice member without company to check for payment and show successful/skipped lines

--- contract_mass_action_invoice_member/wizard/invoice_member_wizard.py
+++ contract_mass_action_invoice_member/wizard/invoice_member_wizard.py
@@ -48,6 +48,18 @@ class InvoiceMemberWizard(models.TransientModel):
         ),
         string="Contracts",
     )
+    successful_contract_line_ids = fields.Many2many(
+        "contract.line",
+        "succesful_contract_rel",
+        string="Succesfully Invoiced Contract Lines",
+        readonly=1,
+    )
+    skipped_contract_line_ids = fields.Many2many(
+        "contract.line",
+        "skipped_contract_rel",
+        string="Skipped Contract Lines",
+        readonly=1,
+    )
 
     # 3. Default methods
 
@@ -60,25 +72,65 @@ class InvoiceMemberWizard(models.TransientModel):
     # 7. Action methods
     def action_invoice_member(self):
         for wizard in self:
+            skipped_contract_lines = self.env["contract.line"]
+            successful_contract_lines = self.env["contract.line"]
             for contract in wizard.contract_ids:
-                contract_line_names = []
-                # set invoice address as partner/member
-                contract.partner_invoice_id = contract.partner_id
+                skip_contract = True
                 for contract_line in contract.contract_line_fixed_ids:
+                    skip_contract_line = False
                     # check if contract line is not cancelled
                     if contract_line.state in ["upcoming", "in-progress", "to-renew"]:
-                        # reset next invoicing date as today
-                        contract_line.recurring_next_date = fields.Date.today()
-                        contract_line_names.append(contract_line.name)
-                if contract_line_names:
+                        # skip contract_line if contract_line product membership date end matches with
+                        # a paid membership_line date end
+                        if any(
+                            contract_line.product_id == membership_line.membership_id
+                            and membership_line.date_to
+                            == contract_line.product_id.membership_date_to
+                            and membership_line.state == "paid"
+                            for membership_line in contract.membership_line_ids
+                        ):
+                            skip_contract_line = True
+                            skipped_contract_lines += contract_line
+                        # if any line lis not skipped reset next_invoicing date as today and
+                        # run contract.recurring_create_invoice()
+                        if not skip_contract_line:
+                            skip_contract = False
+                            contract_line.recurring_next_date = fields.Date.today()
+                            successful_contract_lines += contract_line
+                # if contract is not skipped reset next_invoicing date as today \
+                # and run contract.recurring_create_invoice()
+                if not skip_contract:
+                    # set invoice address as partner/member
+                    contract.partner_invoice_id = contract.partner_id
                     contract.recurring_next_date = fields.Date.today()
-                    _logger.info(
-                        _(
-                            "Following contract lines invoiced for a contract %s:\n%s",
-                            contract.name,
-                            contract_line_names,
-                        )
+                    contract.recurring_create_invoice()
+            _logger.info(
+                _(
+                    "Invoice Member Wizard Without Company run succesfully.\n "
+                    "Succesful Contract Lines: %s\n Skipped Contract Lines: %s"
+                    % (successful_contract_lines, skipped_contract_lines)
+                )
+            )
+            return {
+                "type": "ir.actions.act_window",
+                "name": _("Success"),
+                "context": {
+                    "default_successful_contract_line_ids": successful_contract_lines.ids,
+                    "default_skipped_contract_line_ids": skipped_contract_lines.ids,
+                },
+                "view_type": "form",
+                "view_mode": "form",
+                "res_model": "invoice.member.wizard",
+                "views": [
+                    (
+                        self.env.ref(
+                            "contract_mass_action_invoice_member.contract_mass_action_invoice_member_form_log"
+                        ).id,
+                        "form",
                     )
-                contract.recurring_create_invoice()
+                ],
+                "target": "new",
+            }
 
-    # 8. Business methods
+
+# 8. Business methods
