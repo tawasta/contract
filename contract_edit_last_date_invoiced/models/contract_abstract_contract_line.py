829c36ccf7c88517cce85cbaef91e0917a1dfe81
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-02-09 09:02:00 +0200

    [IMP] Allow delaying the invoice date

--- contract_edit_last_date_invoiced/models/contract_abstract_contract_line.py
+++ contract_edit_last_date_invoiced/models/contract_abstract_contract_line.py
@@ -9,11 +9,7 @@ class ContractAbstractContractLine(models.AbstractModel):
     @api.depends("next_period_date_start")
     def _compute_recurring_next_date(self):
         for rec in self:
-            if (
-                rec.last_date_invoiced
-                and rec.recurring_next_date
-                and rec.last_date_invoiced >= rec.recurring_next_date
-            ):
+            if rec.last_date_invoiced and rec.recurring_next_date:
                 rec.last_date_invoiced = rec.recurring_next_date + relativedelta(
                     days=-1
                 )
