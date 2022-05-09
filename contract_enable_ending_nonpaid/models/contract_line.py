0bebdf3f5370e68e6d33ff80ac450c69836c0c9c
Miika Nissi <miika@miikanissi.com>
2022-05-09 20:17:06 +0300

    [MIG] contract_enable_ending_nonpaid 12 to 14

---
+++ contract_enable_ending_nonpaid/models/contract_line.py
@@ -0,0 +1,9 @@
+from odoo import fields, models
+
+
+class ContractLine(models.Model):
+    _inherit = "contract.line"
+
+    last_date_invoiced = fields.Date(
+        string="Invoiced until", readonly=False, copy=False
+    )
