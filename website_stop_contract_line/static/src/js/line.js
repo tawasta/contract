5328dd5844bcfd67511cb1e9c87b25afefe6fb7b
Miika Nissi <miika@miikanissi.com>
2022-08-23 19:46:24 +0300

    [REF] PRE COMMIT

--- website_stop_contract_line/static/src/js/line.js
+++ website_stop_contract_line/static/src/js/line.js
@@ -1,19 +1,19 @@
 odoo.define("website_stop_contract_line.portal_contract_page", function () {
-  "use strict";
+    "use strict";
 
-  $(function () {
-    $("#cancelModal").on("show.bs.modal", function (e) {
-      var removeLine = $(e.relatedTarget).data("line-id");
-      $(e.currentTarget).find('input[name="stop_line_id"]').val(removeLine);
-    });
+    $(function () {
+        $("#cancelModal").on("show.bs.modal", function (e) {
+            var removeLine = $(e.relatedTarget).data("line-id");
+            $(e.currentTarget).find('input[name="stop_line_id"]').val(removeLine);
+        });
 
-    $(document).on("click", ".stop-confirm", function () {
-      var lineValue = document.getElementsByName("stop_line_id")[0].value;
-      var action = "/contract/line/stop/" + lineValue;
-      $("#cancelModal").modal("hide");
-      $.get(action).done(function () {
-        location.reload();
-      });
+        $(document).on("click", ".stop-confirm", function () {
+            var lineValue = document.getElementsByName("stop_line_id")[0].value;
+            var action = "/contract/line/stop/" + lineValue;
+            $("#cancelModal").modal("hide");
+            $.get(action).done(function () {
+                location.reload();
+            });
+        });
     });
-  });
 });
