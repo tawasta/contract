0efb9856a7fa78c91e9135d0ec94180ef01d480d
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2023-01-03 15:20:42 +0200

    [MIG] 14.0 contract_show_period_on_lines
    Automatically show invoice period on lines

--- contract_show_period_on_lines/models/contract_contract.py
+++ contract_show_period_on_lines/models/contract_contract.py
@@ -9,43 +9,5 @@ class ContractContract(models.Model):
         string="Show Invoice Period on Lines",
         help="If this is checked, each generated invoice line will \
         also show the time span of the invoice",
+        default=True
     )
-
-    invoicing_period_date_from = fields.Date(
-        string="Next period start",
-        compute="_compute_invoice_period",
-    )
-    invoicing_period_date_to = fields.Date(
-        string="Next period end",
-        compute="_compute_invoice_period",
-    )
-
-    @api.depends('contract_line_ids')
-    def _compute_invoice_period(self):
-        for record in self:
-            if not record.contract_line_ids:
-                record.invoicing_period_date_from = False
-                record.invoicing_period_date_to = False
-
-            for line in record.contract_line_ids:
-                date_format = "%Y-%m-%d"
-
-                self.env["contract.line"]
-
-                dates = line._get_period_to_invoice(
-                    record.last_date_invoiced, record.recurring_next_date
-                )
-
-                first_date_invoiced = dates[0].strftime(date_format)
-                last_date_invoiced = dates[1].strftime(date_format)
-
-                if record.show_period_on_lines:
-
-                    # Add the invoicing period after line
-                    record.invoicing_period_date_from = first_date_invoiced
-                    record.invoicing_period_date_to = last_date_invoiced
-                else:
-                    record.invoicing_period_date_from = False
-                    record.invoicing_period_date_to = False
-
-                break
