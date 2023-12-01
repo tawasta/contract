7cd7034f908e836f013892bcea5b43902eaee6a2
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-12-01 12:12:27 +0200

    Added new fields

--- upload_and_generate_contracts/__manifest__.py
+++ upload_and_generate_contracts/__manifest__.py
@@ -28,13 +28,7 @@
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "membership",
-        "contacts",
-        "contract",
-        "account",
-        "sale_generate_membership",
-    ],
+    "depends": ["membership", "contacts", "contract", "account", "sale_generate_membership"],
     "external_dependencies": {"python": ["openpyxl"]},
     "data": [
         "security/ir.model.access.csv",
