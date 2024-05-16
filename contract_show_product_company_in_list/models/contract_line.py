a3a1d52a5f0daf76c5c1ad38cd18422a62cacf8f
odooProg <kalle.rantalainen01@gmail.com>
2024-05-16 13:30:44 +0300

    [FIX] run precommit

--- contract_show_product_company_in_list/models/contract_line.py
+++ contract_show_product_company_in_list/models/contract_line.py
@@ -1,12 +1,14 @@
-from odoo import fields, models, api
+from odoo import api, fields, models
+
 
 class ContractLine(models.Model):
 
     _inherit = "contract.line"
 
-    company = fields.Many2one(string="Company", comodel_name="res.company", compute="_compute_company")
+    company = fields.Many2one(
+        string="Company", comodel_name="res.company", compute="_compute_company"
+    )
 
-    @api.depends('product_id')
+    @api.depends("product_id")
     def _compute_company(self):
         self.company = self.product_id.variant_company_id
-
