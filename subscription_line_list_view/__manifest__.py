c922d8f7304d4a53702fe5909dea56577469e72c
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-10-09 15:28:19 +0300

    [IMP] Add search view, optional fields. Disable create/edit

--- subscription_line_list_view/__manifest__.py
+++ subscription_line_list_view/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Subscription line list views",
     "summary": "Creates a tree view for subscription lines",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Contract Management",
     "website": "https://gitlab.com/tawasta/odoo/contract",
     "author": "Tawasta",
@@ -30,6 +30,7 @@
     "installable": True,
     "depends": ["subscription_oca", "subscription_line_partner"],
     "data": [
+        "views/subscription_line_search.xml",
         "views/subscription_lines.xml",
     ],
 }
