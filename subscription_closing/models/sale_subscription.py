845c5aec4218be7cef78ca03cccba44eb5fe4872
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-10-15 09:26:35 +0300

    [FIX] Call super correctly

--- subscription_closing/models/sale_subscription.py
+++ subscription_closing/models/sale_subscription.py
@@ -7,7 +7,7 @@ class SaleSubscription(models.Model):
     _inherit = "sale.subscription"
 
     def action_close_subscription(self):
-        res = super()
+        res = super().action_close_subscription()
 
         stage = self.stage_id
         closed_stage = self.env["sale.subscription.stage"].search(
