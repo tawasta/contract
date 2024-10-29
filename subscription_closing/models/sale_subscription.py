93a9a61d4d748962514f9b77929321ba1adb0a2c
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-10-29 10:31:27 +0200

    [IMP] Set subscription end time to lines, when closing a subscription

--- subscription_closing/models/sale_subscription.py
+++ subscription_closing/models/sale_subscription.py
@@ -6,6 +6,21 @@ from odoo import models
 class SaleSubscription(models.Model):
     _inherit = "sale.subscription"
 
+    def write(self, vals):
+        res = super().write(vals)
+
+        if "date" in vals and hasattr(self.env["sale.subscription.line"], "date_end"):
+            # Set end date to all subscription lines that haven't ended already
+            for record in self:
+                record.sale_subscription_line_ids.write(
+                    {
+                        "date_end": vals["date"],
+                        "active": False,
+                    }
+                )
+
+        return res
+
     def cron_subscription_management(self):
         res = super().cron_subscription_management()
         for subscription in self.search([("date", "<=", fields.Date.today())]):
@@ -15,10 +30,9 @@ class SaleSubscription(models.Model):
                 closed_stage = self.env["sale.subscription.stage"].search(
                     [("type", "=", "post")], limit=1
                 )
-                print(stage)
-                print(closed_stage)
                 if stage != closed_stage:
                     subscription.stage_id = closed_stage
+                    # Auto-archive
                     # self.active = False
 
         return res
