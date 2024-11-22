ddcdc73b3d6f57213c6d85c66130e6af7389d791
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-11-22 09:55:02 +0200

    [IMP] - created a new module which allows users to upgrade their subscription line

---
+++ website_upgrade_subscription_line/controllers/main.py
@@ -0,0 +1,65 @@
+from odoo import http, _
+from odoo.http import request
+import json
+
+class SubscriptionController(http.Controller):
+
+    @http.route(['/subscription/line/<int:line_id>/upgrade'], type='http', auth='user', website=True, methods=['POST'])
+    def upgrade_subscription_line(self, line_id, **post):
+        response = {"error": False, "msg": _("Subscription line upgraded successfully!")}
+
+        try:
+            line = request.env['sale.subscription.line'].browse(line_id)
+            if not line:
+                response.update({"error": True, "msg": _("Subscription line not found.")})
+                return json.dumps(response)
+
+            new_product_id = int(post.get('new_product', 0))
+            if not new_product_id:
+                response.update({"error": True, "msg": _("No new product selected.")})
+                return json.dumps(response)
+
+            subscription = line.sale_subscription_id
+            if not subscription:
+                response.update({"error": True, "msg": _("No subscription found for this line.")})
+                return json.dumps(response)
+
+            line.action_stop()
+
+            new_line_vals = {
+                'sale_subscription_id': subscription.id,
+                'product_id': new_product_id,
+                'name': request.env['product.product'].browse(new_product_id).name,
+                'product_uom_qty': line.product_uom_qty,
+                'price_unit': request.env['product.product'].browse(new_product_id).lst_price,
+            }
+
+            new_line = request.env['sale.subscription.line'].create(new_line_vals)
+            new_line.action_start()
+
+        except Exception as e:
+
+            response.update({"error": True, "msg": _("An error occurred while upgrading the subscription line.")})
+
+        return json.dumps(response)
+
+
+    @http.route(['/subscription/line/<int:line_id>/upgrade/modal'], type='json', auth='user', website=True)
+    def get_upgrade_modal(self, line_id):
+        line = request.env['sale.subscription.line'].browse(line_id)
+        if not line:
+            return False
+
+        line_product_id = line.product_id
+
+        products = (
+            request.env["product.template"]
+            .sudo()
+            .search([("id", "=", line_product_id.product_tmpl_id.id)])
+            .mapped("product_variant_ids")
+        )
+        
+        return request.env['ir.ui.view']._render_template('website_upgrade_subscription_line.subscription_line_upgrade_modal', {
+            'line_id': line.id,
+            'products': products,
+        })
---
+++ website_upgrade_subscription_line/static/src/js/main.esm.js
@@ -0,0 +1,131 @@
+/** @odoo-module **/
+
+import publicWidget from "@web/legacy/js/public/public_widget";
+import { jsonrpc } from "@web/core/network/rpc_service";
+import Dialog from "@web/legacy/js/core/dialog";
+import {_t} from "@web/core/l10n/translation";
+
+var SubscriptionLineUpgrade = publicWidget.Widget.extend({
+    selector: '#item_details',
+    events: {
+        'click #modal_subscription_line_upgrade': '_onClickUpgradeButton',
+    },
+
+    start: function () {
+        this._super.apply(this, arguments);
+        $(document).on("submit", "#subscription_line_upgrade_form", this._onFormSubmit.bind(this));
+    },
+
+    _onClickUpgradeButton: function (ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+        const lineId = $(ev.currentTarget).attr('line-id');
+
+        if (!lineId) {
+            console.error("Line ID not found.");
+            return;
+        }
+
+        jsonrpc(`/subscription/line/${lineId}/upgrade/modal`, {}).then(function (modalContent) {
+            const $modal = $(modalContent);
+            $modal.find('.modal-body > div').removeClass('container');
+            $modal.appendTo(document.body);
+            const modalBS = new Modal($modal[0], { backdrop: 'static', keyboard: false });
+            modalBS.show();
+
+            $modal.on('click', '.btn-close', function () {
+                $modal.remove();
+            });
+            $modal.on('hidden.bs.modal', function () {
+                $modal.remove();
+            });
+        }).catch(function (err) {
+            console.error("Failed to load modal content", err);
+        });
+    },
+
+    _onFormSubmit: function (ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+
+        const $form = $(ev.currentTarget);
+        const actionUrl = $form.attr('action');
+        const formData = new FormData($form[0]);
+        this._showLoadingScreen();
+
+        $.ajax({
+            url: actionUrl,
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: (response) => {
+                const jsonResponse = JSON.parse(response);
+
+                if (jsonResponse.error) {
+                    this._showErrorMessage(jsonResponse.msg);
+                } else {
+                    this._showSuccessMessage(jsonResponse.msg);
+                    $form.closest('.modal').modal('hide');
+                }
+            },
+            error: (err) => {
+                console.error("Form submission failed:", err);
+                this._showErrorMessage("An unexpected error occurred.");
+            },
+            complete: () => {
+                this._hideLoadingScreen();
+            },
+        });
+    },
+
+    _showSuccessMessage: function (message) {
+        new Dialog(this, {
+            title: _t("Success"),
+            size: "medium",
+            $content: $("<div/>").html(message),
+            buttons: [
+                {
+                    text: _t("OK"),
+                    close: true,
+                    click: function () {
+                        location.reload();
+                    },
+                },
+            ],
+        }).open();
+    },
+
+    _showLoadingScreen: function () {
+        const loadingMessage = `
+            <div id="loading-screen" style="
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-color: rgba(0, 0, 0, 0.5);
+                z-index: 9999;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                color: #fff;
+                font-size: 20px;">
+                <div>
+                    <div class="spinner-border text-light" role="status"></div>
+                    <p>Loading, please wait...</p>
+                </div>
+            </div>`;
+        $('body').append(loadingMessage);
+    },
+
+    _hideLoadingScreen: function () {
+        $('#loading-screen').remove();
+    },
+
+    _showErrorMessage: function (message) {
+        alert(`Error: ${message}`);
+    },
+});
+
+publicWidget.registry.SubscriptionLineUpgrade = SubscriptionLineUpgrade;
