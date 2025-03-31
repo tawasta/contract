c03c0b063c1de13e3f5f7f4af1fd27e779090328
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-03-31 16:26:42 +0300

    [IMP] - modified modal and added company toggle

--- website_subscription_partner_edit/static/src/js/main.esm.js
+++ website_subscription_partner_edit/static/src/js/main.esm.js
@@ -22,6 +22,8 @@ var PartnerUpgrade = publicWidget.Widget.extend({
             ".o_wsale_add_address",
             this._onToggleNewContact.bind(this)
         );
+
+        $(document).on("change", "#is_company_toggle", this._onToggleIsCompany.bind(this));
     },
 
     _onClickUpgradeButton: function (ev) {
@@ -67,6 +69,19 @@ var PartnerUpgrade = publicWidget.Widget.extend({
                 console.error("Failed to load modal content", err);
             });
     },
+
+    _onToggleIsCompany: function (ev) {
+        const isChecked = $(ev.currentTarget).is(":checked");
+        console.log(isChecked);
+        const $alvField = $("#company_registry_group");
+        console.log($alvField);
+        if (isChecked) {
+            $alvField.removeClass("d-none");
+        } else {
+            $alvField.addClass("d-none");
+        }
+    },
+
 
     _onSelectContact: function (ev) {
         ev.preventDefault();
