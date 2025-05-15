8870db2bde6a92206308db8d2e0ad1f5bab27c96
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-05-15 10:24:10 +0300

    [IMP] - modified readme

--- website_subscription_partner_edit/models/transmit_method.py
+++ website_subscription_partner_edit/models/transmit_method.py
@@ -5,7 +5,13 @@ class TransmitMethod(models.Model):
     _inherit = "transmit.method"
 
     contract_target_type = fields.Selection(
-        [('person', 'Person'), ('company', 'Company'), ('both', 'Both')],
+        [
+            ('person', 'Person'),
+            ('company', 'Company'),
+            ('both', 'Both'),
+        ],
         default='both',
-        string="Target Type"
+        string="Visible for Contact Type",
+        help="Defines whether this transmit method is selectable for persons, companies, or both in the website subscription contact form."
     )
+
