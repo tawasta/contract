6ce3c41877ea4d90ae49ac28da09ab41d5726513
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-11-11 11:14:21 +0200

    [IMP] - created new module

---
+++ subscription_line_marker/models/sale_subscription_line.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+
+class SaleSubscriptionLine(models.Model):
+    _inherit = "sale.subscription.line"
+
+    NAME_SUFFIX = " (#START# - #END#)"
+
+    def _compute_name(self):
+        result = super()._compute_name()
+
+        for rec in self:
+            if rec.product_id and rec.name:
+                name = rec.name.strip()
+                if self.NAME_SUFFIX not in name:
+                    rec.name = f"{name}{self.NAME_SUFFIX}"
+
+        return result
