1862b40d00911d6e26b9fbcee42334179550b97e
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-09-02 08:37:18 +0300

    [MIG] 19.0 contract_invoice_address

---
+++ contract_invoice_address/README.rst
@@ -0,0 +1,45 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=====================================================
+Contract Invoice Address and force Commercial Partner
+=====================================================
+
+Add invoice address to contracts and assign its commercial partner to an invoice
+if needed with a setting.
+
+Configuration
+=============
+Go to Settings -> Invoicing and select "Force commercial partner on contracts"
+to set commercial partner to an invoice to be created.
+
+Usage
+=====
+Select a contract and test its invoice creation. Use "Force commercial partner
+on contracts" setting to see that the invoice gets the commercial partner of
+an invoice address of contract.
+
+Known issues / Roadmap
+======================
+\-
+
+Credits
+=======
+
+Contributors
+------------
+
+* Valtteri Lattu <valtteri.lattu@futural.fi>
+* Miika Nissi <miika.nissi@futural.fi>
+* Timo Kekäläinen <timo.kekalainen@futural.fi>
+* Joona Isoaho <joona.isoaho@futural.fi>
+
+Maintainer
+----------
+
+.. image:: https://futural.fi/templates/tawastrap/images/logo.png
+   :alt: Futural Oy
+   :target: https://futural.fi/
+
+This module is maintained by Futural Oy
---
+++ contract_invoice_address/models/contract_contract.py
@@ -0,0 +1,63 @@
+from odoo import api, fields, models
+
+
+class Contract(models.Model):
+    _inherit = "contract.contract"
+
+    partner_invoice_id = fields.Many2one(
+        comodel_name="res.partner", string="Invoice Address"
+    )
+    invoice_for_company = fields.Boolean(
+        "Invoice for a company",
+        compute="_compute_invoice_for_company",
+        help="Invoice goes for a company",
+        store=True,
+    )
+
+    separate_invoice_address = fields.Boolean(
+        "Separate invoice address",
+        help="Customer and invoice address differ",
+        compute="_compute_separate_invoice_address",
+        store=True,
+    )
+
+    @api.depends("partner_id", "partner_invoice_id")
+    def _compute_invoice_for_company(self):
+        for record in self:
+            partner = record.partner_invoice_id or record.partner_id
+            record.invoice_for_company = partner.is_company
+
+    @api.depends("partner_invoice_id", "partner_id")
+    def _compute_separate_invoice_address(self):
+        for record in self:
+            record.separate_invoice_address = (
+                record.partner_invoice_id
+                and record.partner_id != record.partner_invoice_id
+            )
+
+    def _prepare_invoice(self, *args, **kwargs):
+        # Super will handle ensure_one() and other validation
+        invoice_vals = super()._prepare_invoice(*args, **kwargs)
+
+        # Add shipping address to writable values
+        if self.partner_invoice_id:
+            partner = self.partner_invoice_id
+        else:
+            partner = self.partner_id
+
+        if (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("contract_force_commercial_partner")
+        ):
+            if (
+                self.partner_invoice_id.type != "invoice"
+                and self.partner_invoice_id
+                != self.partner_invoice_id.commercial_partner_id
+            ):
+                # Force commercial partner
+                partner = partner.commercial_partner_id
+
+        invoice_vals["partner_id"] = partner.id
+
+        return invoice_vals
